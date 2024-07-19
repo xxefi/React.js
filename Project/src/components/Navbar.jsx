3a8dd5a305b1d8c09b8824b1c9577019d2dd653b
Afgan <134388584+xxefi@users.noreply.github.com>
2024-07-19 05:01:18 +0400

    Update Navbar.jsx

--- Project/src/components/Navbar.jsx
+++ Project/src/components/Navbar.jsx
@@ -14,7 +14,6 @@ export default function Navbar() {
                 ECOMMERCE
               </Text>
             </Link>
-
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
               <div className="flex flex-shrink-0 items-center">
                 <Link to="/">
