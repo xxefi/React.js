3538df399ac01b73edef41c839e304a6ab3626f8
Afgan <134388584+xxefi@users.noreply.github.com>
2024-07-20 04:26:11 +0400

    Delete Project directory

--- Project/src/components/nav-items/NavLeftItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function NavLeftItems({ Link, Text }) {
-  return (
-    <Link to="/">
-      <Text color="white" fontSize="25px">
-        ECOMMERCE
-      </Text>
-    </Link>
-  );
-}
